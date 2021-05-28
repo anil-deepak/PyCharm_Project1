ab432fd48d36746515a43210693e3478fdf6fb65
Anil Deepak Talathoti <78140366+anil-deepak@users.noreply.github.com>
2021-05-27 22:25:04 -0700

    multiple values

---
+++ Lib/multival.py
@@ -0,0 +1,14 @@
+#multiple values to many variables
+a,b,c='apple',10,20
+print(a)
+print(b+c)
+
+#multiple variables sinle value
+x=y=z="Orange"
+
+print(y)
+
+#unpacking a collection
+fruits=['apple','orange','pineapple']
+x,y,z=fruits
+print(z)
